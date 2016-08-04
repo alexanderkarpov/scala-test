7824630590cc770f11bbbd815dbfc3b31fc931c9
alexander <alexander.karpoff@gmail.com>
2016-08-04 17:22:37 +0300

    BinaryTree.foreach

--- src/main/scala/com/example/App.scala
+++ src/main/scala/com/example/App.scala
@@ -4,9 +4,9 @@ object App {
 
   def main(args: Array[String]) {
 
-    val binaryTree: BinaryTree = new Node(10, new Empty, new Empty)
+    val binaryTree: BinaryTree = new Node(10, new Empty, new Empty).incl(12).incl(9).incl(4).incl(15).incl(2)
 
-    println(binaryTree.incl(12).incl(9).incl(4).incl(15).incl(2))
+    binaryTree.foreach(i => print("<" + i + ">"))
   }
 
 }
--- src/main/scala/com/example/BinaryTree.scala
+++ src/main/scala/com/example/BinaryTree.scala
@@ -4,21 +4,36 @@ abstract class BinaryTree {
 
   def incl(newElem: Int): BinaryTree
 
+  def foreach(f: Int => Unit)
+
+//  def filter(p: Int => Boolean): BinaryTree
+
 }
 
 class Empty extends BinaryTree {
   override def incl(newElem: Int): BinaryTree = new Node(newElem, new Empty, new Empty)
 
+  override def foreach(f: (Int) => Unit): Unit = {}
+
+//  override def filter(p: (Int) => Boolean): BinaryTree = ???
+
   override def toString: String = ""
 }
 
 class Node(elem: Int, left: BinaryTree, right: BinaryTree) extends BinaryTree {
+
   override def incl(newElem: Int): BinaryTree =
     if (newElem == elem) this
     else if (newElem < elem) new Node(elem, left incl newElem, right)
     else new Node(elem, left, right incl newElem)
 
-  override def toString: String = left.toString + "[" + elem + "] " + right.toString
+  override def foreach(f: (Int) => Unit): Unit = {
+    left.foreach(f)
+    f.apply(elem)
+    right.foreach(f)
+  }
+
+  override def toString: String =  left.toString + " " + elem + " " + right.toString
 }
 
 

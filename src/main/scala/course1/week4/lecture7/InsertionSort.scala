034599b7527ed1f8e695b2a299146655306e57cb
Alexander Karpov <alexander.karpoff@gmail.com>
2016-08-14 08:57:22 +0300

    [1.4.7] insertion sort

---
+++ src/main/scala/course1/week4/lecture7/InsertionSort.scala
@@ -0,0 +1,18 @@
+package course1.week4.lecture7
+
+/**
+  * Created by alexander on 8/14/16.
+  */
+object InsertionSort extends App {
+
+  def isort(xs: List[Int]): List[Int] = xs match {
+    case List() => List()
+    case y :: ys => insert(y, isort(ys))
+  }
+
+  def insert(x: Int, xs: List[Int]): List[Int] = xs match {
+    case List() => List(x)
+    case y :: ys => if (x <= y) x :: xs else y :: insert(x, ys)
+  }
+
+}

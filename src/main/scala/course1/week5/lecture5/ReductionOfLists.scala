048efd41dcf5754289097af8700c38a06b160745
Alexander Karpov <alexander.karpoff@gmail.com>
2016-08-20 12:54:57 +0300

    [1.5.5] Reduction of the lists

--- src/main/scala/course1/week5/lecture5/ReductionOfLists.scala
+++ src/main/scala/course1/week5/lecture5/ReductionOfLists.scala
@@ -3,9 +3,11 @@ package course1.week5.lecture5
 
 object ReductionOfLists extends App {
 
-  def sum(xs: List[Int]): Int = (0 :: xs) reduceLeft ((x, y) => x + y)
+  //  def sum(xs: List[Int]): Int = (0 :: xs) reduceLeft (_ + _) // ((x, y) => x + y)
+  def sum(xs: List[Int]): Int = xs.foldLeft(0)(_ + _) // ((x, y) => x + y)
 
-  def product(xs: List[Int]): Int = (1 :: xs) reduceLeft ((x, y) => x * y)
+  //  def product(xs: List[Int]): Int = (1 :: xs) reduceLeft (_ * _) // ((x, y) => x * y)
+  def product(xs: List[Int]): Int = (xs foldLeft 1) (_ * _)
 
   val list = List(1, 2, 3, 4, 5)
   //  val list = List()
@@ -13,5 +15,13 @@ object ReductionOfLists extends App {
   println(sum(list))
   println(product(list))
 
+  def mapFun[T, U](xs: List[T], f: T => U): List[U] =
+    (xs foldRight List[U]()) ((x, y) => List(f.apply(x)) ::: y)
+
+  def lengthFun[T](xs: List[T]): Int =
+    (xs foldRight 0) ((x, y) => y + 1)
+
+  println(lengthFun(list))
+  println(mapFun[Int, Double](list, x => x / 2.0))
 
 }

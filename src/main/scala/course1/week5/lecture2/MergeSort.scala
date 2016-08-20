57ea4535eedae8ac47e3edc3cdcfa32fcb754382
Alexander Karpov <alexander.karpoff@gmail.com>
2016-08-20 08:19:45 +0300

    [1.5.3] Parametrized Merge Sort

--- src/main/scala/course1/week5/lecture2/MergeSort.scala
+++ src/main/scala/course1/week5/lecture2/MergeSort.scala
@@ -12,23 +12,24 @@ package course1.week5.lecture2
   */
 object MergeSort extends App {
 
-  def msort(xs: List[Int]): List[Int] = {
+  def msort[T](xs: List[T])(lt: (T, T) => Boolean): List[T] = {
     val n = xs.length / 2
     if (n == 0) xs
     else {
 
-      def merge(xs: List[Int], ys: List[Int]): List[Int] = (xs, ys) match {
+      def merge(xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
         case (Nil, ys) => ys
         case (xs, Nil) => xs
-        case (x :: xs1, y :: ys1) => if(x < y) x :: merge(xs1, ys) else y :: merge(ys1, xs)
+        case (x :: xs1, y :: ys1) => if (lt(x, y)) x :: merge(xs1, ys) else y :: merge(ys1, xs)
       }
 
       val (fst, snd) = xs splitAt n
-      merge(msort(fst), msort(snd))
+      merge(msort(fst)(lt), msort(snd)(lt))
     }
   }
 
+  //  def readyToSort = msort(List(3, 6, 3, 756, 3444, 73, 23, 64, 710, 3, 0, 1, -9, -53, -32, 64));
 
-  println(msort(List(3, 6, 3, 756, 3444, 73, 23, 64, 710, 3, 0, 1, -9, -53, -32, 64)))
+  println(msort(List(3, 6, 3, 756, 3444, 73, 23, 64, 710, 3, 0, 1, -9, -53, -32, 64))((x, y) => x < y))
 
 }

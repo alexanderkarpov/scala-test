87e743c5867f0e45d00f03744a4800752cc20ff1
Alexander Karpov <alexander.karpoff@gmail.com>
2016-08-20 08:01:11 +0300

    [1.5.2] Merge Sort

--- src/main/scala/course1/week5/lecture2/MergeSort.scala
+++ src/main/scala/course1/week5/lecture2/MergeSort.scala
@@ -16,15 +16,13 @@ object MergeSort extends App {
     val n = xs.length / 2
     if (n == 0) xs
     else {
-      def merge(xs: List[Int], ys: List[Int]): List[Int] = xs match {
-        case Nil => ys
-        case x :: xs1 => ys match {
-          case Nil => xs
-          case y :: ys1 =>
-            if (x < y) x :: merge(xs1, ys)
-            else y :: merge(xs, ys1)
-        }
+
+      def merge(xs: List[Int], ys: List[Int]): List[Int] = (xs, ys) match {
+        case (Nil, ys) => ys
+        case (xs, Nil) => xs
+        case (x :: xs1, y :: ys1) => if(x < y) x :: merge(xs1, ys) else y :: merge(ys1, xs)
       }
+
       val (fst, snd) = xs splitAt n
       merge(msort(fst), msort(snd))
     }

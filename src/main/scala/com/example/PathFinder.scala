a72713e0e335d144506fea3aa7b7657f7f017480
alexander <alexander.karpoff@gmail.com>
2016-09-28 17:00:57 +0300

    [path finder] multi-leafs tree

--- src/main/scala/com/example/PathFinder.scala
+++ src/main/scala/com/example/PathFinder.scala
@@ -1,18 +1,20 @@
 package com.example
 
+import java.util.concurrent.atomic.AtomicLong
+
 object PathFinder extends App {
 
 
   def totalWeight(root: TreeNode): Double = {
 
     def loop(node: TreeNode, acc: Double): Double = {
-      if(node == null) acc
+      if (node == null) acc
       else node.nodes match {
         case Nil => node.weight + acc
-        case head::Nil => loop(head, node.weight) + acc
-        case head::tail => {
+        case head :: Nil => loop(head, node.weight) + acc
+        case head :: tail => {
           val headNodeWeight = loop(head, node.weight)
-          val middleNodesWeights: List[Double] = (for(i <- (0 until tail.length - 1)) yield loop(tail(i), 0.0)).toList
+          val middleNodesWeights: List[Double] = (for (i <- (0 until tail.length - 1)) yield loop(tail(i), 0.0)).toList
           val lastNodeWeight = loop(tail.last, acc)
           headNodeWeight + middleNodesWeights.sum + lastNodeWeight
         }
@@ -22,24 +24,30 @@ object PathFinder extends App {
     loop(root, 0.0)
   }
 
- def Node(weight: Double, children: TreeNode*) = TreeNode(weight, children.toList)
+  def getAllPaths(root: TreeNode): Set[List[TreeNode]] = {
+
+    def loop(node: TreeNode, acc: Set[List[TreeNode]]): Set[List[TreeNode]] = ???
+
+    loop(root, Set())
+
+  }
+
+  val id = new AtomicLong
+
+  def nextId = id.incrementAndGet
+
+  def Node(weight: Double, children: TreeNode*) = TreeNode(nextId, weight, children.toList)
 
   val tree = Node(1, Node(2, Node(3), Node(3), Node(3), Node(3)), Node(2, Node(3), Node(3)), Node(2, Node(3), Node(2)))
 
   println(totalWeight(tree))
 
-//  val smallTree = TreeNode(1, TreeNode(1, null, null), TreeNode(1, null, null))
-//  println(totalWeight(smallTree))
-//
-//  val largeTree = TreeNode(1, TreeNode(2, TreeNode(3, TreeNode(4, TreeNode(5, TreeNode(6, null, null), null), TreeNode(7, null, null)), null), TreeNode(8, TreeNode(9, null, null), TreeNode(10, null, null))), TreeNode(11, TreeNode(12, TreeNode(13, null, TreeNode(14, null, null)), null), TreeNode(15, TreeNode(16, null, null), TreeNode(17, TreeNode(18, null, null), TreeNode(19, TreeNode(20, null, null), null)))))
-//
-//  println(totalWeight(largeTree))
-
+  println(tree)
 
 }
 
-case class TreeNode(weight: Double, nodes: List[TreeNode]) {
-  def this(weight: Double) = this(weight, Nil)
+case class TreeNode(id: Long, weight: Double, nodes: List[TreeNode]) {
+  override def toString: String = s"[id=$id, weight=$weight, {${nodes.mkString(",")}}"
 }
 
 case class CheckPoint(id: Long, nextId: Long, weight: Double)

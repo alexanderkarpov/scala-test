e9db038a5b0e2250b70a0d4d67f11a795b4794ca
alexander <alexander.karpoff@gmail.com>
2016-09-28 15:31:52 +0300

    [path finder] 3-leafs tree

--- src/main/scala/com/example/PathFinder.scala
+++ src/main/scala/com/example/PathFinder.scala
@@ -7,23 +7,30 @@ object PathFinder extends App {
 
     def loop(node: TreeNode, acc: Double): Double = {
       if(node == null) acc
-      else loop(node.leftNode, node.weight) + loop(node.rightNode, acc)
+      else loop(node.leftNode, node.weight) + loop(node.middleNode, 0.0) + loop(node.rightNode, acc)
     }
 
     loop(root, 0.0)
   }
 
-  val smallTree = TreeNode(1, TreeNode(1, null, null), TreeNode(1, null, null))
+  val smallTree = TreeNode(1,
+    TreeNode(1, TreeNode(5, null, TreeNode(5, null, null, null), null), TreeNode(10, null, TreeNode(1, null, null, null), TreeNode(1, null, null, null)), null),
+    TreeNode(1, TreeNode(1, null, TreeNode(1, null, null, null), null), TreeNode(1, null, TreeNode(1, null, TreeNode(1, null, null, null), null), null), TreeNode(1, null, null, null)),
+    TreeNode(1, TreeNode(1, TreeNode(5, null, null, null), TreeNode(5, null, TreeNode(5, null, null, null), null), null), TreeNode(1, null, null, null), TreeNode(1, null, TreeNode(5, null, null, null), null)));
+
   println(totalWeight(smallTree))
 
-  val largeTree = TreeNode(1, TreeNode(2, TreeNode(3, TreeNode(4, TreeNode(5, TreeNode(6, null, null), null), TreeNode(7, null, null)), null), TreeNode(8, TreeNode(9, null, null), TreeNode(10, null, null))), TreeNode(11, TreeNode(12, TreeNode(13, null, TreeNode(14, null, null)), null), TreeNode(15, TreeNode(16, null, null), TreeNode(17, TreeNode(18, null, null), TreeNode(19, TreeNode(20, null, null), null)))))
-
-  println(totalWeight(largeTree))
+//  val smallTree = TreeNode(1, TreeNode(1, null, null), TreeNode(1, null, null))
+//  println(totalWeight(smallTree))
+//
+//  val largeTree = TreeNode(1, TreeNode(2, TreeNode(3, TreeNode(4, TreeNode(5, TreeNode(6, null, null), null), TreeNode(7, null, null)), null), TreeNode(8, TreeNode(9, null, null), TreeNode(10, null, null))), TreeNode(11, TreeNode(12, TreeNode(13, null, TreeNode(14, null, null)), null), TreeNode(15, TreeNode(16, null, null), TreeNode(17, TreeNode(18, null, null), TreeNode(19, TreeNode(20, null, null), null)))))
+//
+//  println(totalWeight(largeTree))
 
 
 }
 
-case class TreeNode(weight: Double, leftNode: TreeNode, rightNode: TreeNode) {
+case class TreeNode(weight: Double, leftNode: TreeNode, middleNode: TreeNode, rightNode: TreeNode) {
   def hasLeftNode = leftNode != null
 
   def hasRightNode = rightNode != null

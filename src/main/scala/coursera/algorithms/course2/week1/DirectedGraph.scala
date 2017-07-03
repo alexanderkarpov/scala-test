df22f2e3217e5b3e478579dfc8937d56924bc46b
Alexander Karpov <alexander.karpoff@gmail.com>
2017-07-04 00:25:09 +0300

    [algo-2] week1: directed graph DFS

--- src/main/scala/coursera/algorithms/course2/week1/DirectedGraph.scala
+++ src/main/scala/coursera/algorithms/course2/week1/DirectedGraph.scala
@@ -1,6 +1,7 @@
 package coursera.algorithms.course2.week1
 
 import scala.collection.mutable
+import scala.io.Source
 
 case class DirectedGraph(vertices: Map[Int, List[Int]]) {
 
@@ -36,6 +37,36 @@ case class DirectedGraph(vertices: Map[Int, List[Int]]) {
     }
 
     loop(startVertex, Nil)
+  }
+
+}
+
+object DirectedGraph {
+
+  def main(args: Array[String]): Unit = {
+    val srcFileName = "src/main/resources/algorithms/scc.txt"
+    val graph = DirectedGraph.fromFile(srcFileName)
+    println(graph.vertices.size)
+  }
+
+  def fromFile(path: String): DirectedGraph = {
+    println(s"loading data from $path ...")
+    val t0 = System.currentTimeMillis
+    val vertices: Map[Int, List[Int]] =
+      Source.fromFile(path).getLines
+        .toList
+        .map(_.split(" "))
+        .map(items => (items(0), items(1)))
+        .map(p => (Integer.parseInt(p._1), Integer.parseInt(p._2)))
+        .groupBy(_._1)
+        .map(e => (e._1, e._2.map(_._2)))
+
+    val t1 = System.currentTimeMillis
+    println(s"data loaded from file in ${(t1 - t0) / 1000} sec")
+    val graph = DirectedGraph(vertices)
+    val t2 = System.currentTimeMillis
+    println(s"graph created in ${(t2 - t1) / 1000} sec")
+    graph
   }
 
 }
